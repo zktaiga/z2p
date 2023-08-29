867b96887fa53770ad0d3f8b9b822f1311f23878
Taiga <taiga@null.net>
2023-08-29 19:16:02 +0400

    Refactor and add tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use z2p::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }

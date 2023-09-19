34d2d690b21d22a134bbc344178ddedb62b4e3c5
Taiga <taiga@null.net>
2023-09-19 21:23:27 +0400

    rust fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,14 @@
 use std::net::TcpListener;
-#[tokio::test]
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = z2p::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("127.0.0.1:{}", port)
+}
+
+#[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
     let client = reqwest::Client::new();
@@ -13,10 +21,49 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = z2p::run(listener).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-    format!("127.0.0.1:{}", port)
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let name = "Some Name";
+    let email = "some@name.com";
+    let body = format!(
+        "name={}&email={}",
+        name.replace(" ", "%20"),
+        email.replace("@", "%40"),
+    );
+    let response = client
+        .post(&format!("http://{}/subscriptions", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_missing_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=some%20name", "missing the email"),
+        ("email=some%40email.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("http://{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "API did not fail with a 400 when the error message was {}",
+            error_message
+        )
+    }
 }
